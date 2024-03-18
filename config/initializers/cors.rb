9ce21f8c3af2994e6cdb45c9e325d5a441a239aa
morishitakouki <morimori.plane.0615@gmail.com>
2024-03-18 13:18:01 +0900

    cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "localhost:3000"
+      origins "localhost:3000", "https://rails-react-frontend-blog-e49b147a0c56.herokuapp.com"
   
       resource "*",
         headers: :any,
